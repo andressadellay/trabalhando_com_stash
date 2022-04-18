a9c9c8d4586442251d6388f849d396b8ab1a3b26
[andressadellayagra] <[andressadellayagra@gmail.com]>
2022-04-18 20:27:10 -0300

    enviando stash de Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa{
+    private String nome;
+    private int anoNasc;
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String n){
+        this.nome = n;
+    }
+
+}
